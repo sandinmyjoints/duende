3c613af58d60e979e8b21b25db75c2bfa2041a51
William Bert <william@spanishdict.com>
2019-10-01 22:57:10 -0400

    Make a toy graph.

--- index.js
+++ index.js
@@ -9,15 +9,69 @@ const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
 
 async function main() {
   console.log(`connecting`);
-  const g = traversal().withRemote(
-    new DriverRemoteConnection('ws://localhost:8182/gremlin')
-  );
+  const conn = new DriverRemoteConnection('ws://localhost:8182/gremlin');
+  const g = traversal().withRemote(conn);
 
-  const v1 = await g
-    .addV('person')
-    .property('name', 'marko')
-    .next();
-  console.log(`v1`, v1);
+  try {
+    await g.V().drop().iterate();
+
+    const v1 = await g
+      .addV('headword')
+      .property('canonical_text', 'gráfico')
+      .property('language', 'es');
+    // .next();
+    console.log(`v1`, v1);
+
+    const v2 = await g
+      .addV('headword')
+      .property('canonical_text', 'graph')
+      .property('language', 'en');
+    // .next();
+    console.log(`v2`, v2);
+
+    const v3 = await g
+      .addV('inflection')
+      .property('canonical_text', 'graphs')
+      .property('language', 'en');
+    // .next();
+    console.log(`v3`, v3);
+
+    // const e1 = await g
+    //   .V(gráfico)
+    //   .addE('translation')
+    //   .to(graph)
+    //   .iterate();
+    // console.log(`e1`, e1);
+
+    const e1 = await v1
+      .addE('translation')
+      .to(v2)
+      .iterate();
+    console.log(`e1`, e1);
+
+    // const e2 = await v2
+    //   .addE('translation')
+    //   .to(v1)
+    //   .iterate();
+    // console.log(`e2`, e2);
+
+    const e3 = await g.V().has('canonical_text', 'graph')
+      .addE('plural')
+      .to(v3)
+      .iterate();
+    console.log(`e3`, e1);
+
+    const gráfico = await g.V().has('canonical_text', 'gráfico').next();
+    console.log('query', gráfico);
+
+    // const graph = g.V().has('canonical_text', 'graph');
+    // const graphs = g.V().has('canonical_text', 'graphs');
+
+  } catch (ex) {
+    console.error(ex);
+  } finally {
+    conn.close();
+  }
 }
 
 main();
