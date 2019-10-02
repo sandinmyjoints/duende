a746c0eedaa217525636f2c82cbfffdbe43d1828
William Bert <william@spanishdict.com>
2019-10-02 14:32:26 -0400

    show inbound edges

--- index.js
+++ index.js
@@ -7,6 +7,35 @@ const traversal = gremlin.process.AnonymousTraversalSource.traversal;
 const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
 // const t = gremlin.process.traversal.t
 
+async function addHeadword(props) {
+  const {lang, handle} = props;
+  const g = this;
+  const v1 = await g.addV('Headword')
+  Object.keys(props).forEach(key => {
+    v1.property(key, props[key]);
+  });
+  await v1.next();
+}
+
+async function addString(text) {
+  const g = this;
+  const v1 = g.addV('String').property('text', text);
+
+  return await v1.next();
+}
+
+async function linkStrings(text1, text2, edgeLabel, props = {}) {
+  const g = this;
+  const s1 = g.V().has('String', 'text', text1);
+  const s2 = g.V().has('String', 'text', text2);
+  const e = s1.addE(edgeLabel).to(s2);
+  Object.keys(props).forEach(key => {
+    e.property(key, props[key]);
+  });
+
+  e.next();
+}
+
 async function main() {
   console.log(`connecting`);
   const conn = new DriverRemoteConnection('ws://localhost:8182/gremlin');
@@ -14,60 +43,60 @@ async function main() {
 
   try {
     // Drop existing data.
-    await g.V().drop().iterate();
-
-    const v1 = await g
-      .addV('headword')
-      .property('canonical_text', 'gráfico')
-      .property('language', 'es');
-    // .next();
-    console.log(`v1`, v1);
-
-    const v2 = await g
-      .addV('headword')
-      .property('canonical_text', 'graph')
-      .property('language', 'en');
-    // .next();
-    console.log(`v2`, v2);
-
-    const v3 = await g
-      .addV('inflection')
-      .property('canonical_text', 'graphs')
-      .property('language', 'en');
-    // .next();
-    console.log(`v3`, v3);
-
-    // const e1 = await g
-    //   .V(gráfico)
-    //   .addE('translation')
-    //   .to(graph)
-    //   .iterate();
-    // console.log(`e1`, e1);
-
-    const e1 = await v1
-      .addE('translation')
-      .to(v2)
+    await g
+      .V()
+      .drop()
       .iterate();
-    console.log(`e1`, e1);
 
-    // const e2 = await v2
-    //   .addE('translation')
-    //   .to(v1)
-    //   .iterate();
-    // console.log(`e2`, e2);
+    const texts = [
+      'estoy embarazada',
+      'estoy avergonzada',
+      'i am embarrassed',
+      'i am pregnant',
+      'estar',
+      'embarazado',
+      'embarazar',
+      'avergonzado',
+    ];
 
-    const e3 = await g.V().has('canonical_text', 'graph')
-      .addE('plural')
-      .to(v3)
-      .iterate();
-    console.log(`e3`, e1);
+    const headwords = [
+      'estar',
+      'i',
+      'embarrass',
+      'pregnant',
+      'to be',
+      'embarazado',
+      'embarazar',
+      'avergonzado',
+    ];
 
-    const gráfico = await g.V().has('canonical_text', 'gráfico').next();
-    console.log('query', gráfico);
+    const gAddString = addString.bind(g);
+    texts.forEach(gAddString);
 
-    // const graph = g.V().has('canonical_text', 'graph');
-    // const graphs = g.V().has('canonical_text', 'graphs');
+    const gLinkStrings = linkStrings.bind(g);
 
+    const gAddHeadword = addHeadword.bind(g)
+    headwords.forEach(gAddHeadword)
+
+    function linkStringsToHeadwords() {
+      // iterate through all headwords
+      // if headword is in a string, add a Contains edge from string to headword
+      // g.V().has('text', )
+    }
+
+    // await gLinkStringsToHeadwords()
+
+    await gLinkStrings('estoy embarazada', 'estoy avergonzada', 'Mistake', {
+      reason: 'false cognate',
+      explanation:
+        'Embarazada sounds like embarrassed to English speakers but it does not mean embarrassed.',
+    });
+    await gLinkStrings('estoy avergonzada', 'estar', 'Contains');
+    await gLinkStrings('estoy embarazada', 'estar', 'Contains');
+    await gLinkStrings('estoy avergonzada', 'avergonzado', 'Contains');
+    const inbound = await g.V().has('String', 'text', 'estar').inE().toList()
+    console.log(`inbound relationships to estar`, inbound);
+    console.log(`done.`);
   } catch (ex) {
     console.error(ex);
   } finally {
