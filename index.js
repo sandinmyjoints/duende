85a67a4c97c4aef9e68b42ae2cb8d03906836200
William Bert <william@spanishdict.com>
2019-10-02 15:24:12 -0400

    Prompt to add mistakes.

--- index.js
+++ index.js
@@ -2,26 +2,30 @@
 // See https://tinkerpop.apache.org/docs/current/reference/#gremlin-javascript
 // Server: https://github.com/bricaud/gremlin-server
 
+const prompts = require('prompts');
+
 const gremlin = require('gremlin');
 const traversal = gremlin.process.AnonymousTraversalSource.traversal;
 const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
 // const t = gremlin.process.traversal.t
 
-async function addHeadword(props) {
-  const {lang, handle} = props;
+async function addStringV(text) {
   const g = this;
-  const v1 = await g.addV('Headword')
+  const v1 = g.addV('String').property('text', text);
+  return await v1.iterate();
+}
+
+async function addHeadwordV(props, canonicalStringV) {
+  const { lang } = props;
+  const g = this;
+  const v1 = await g.addV('Headword');
   Object.keys(props).forEach(key => {
     v1.property(key, props[key]);
   });
-  await v1.next();
-}
-
-async function addString(text) {
-  const g = this;
-  const v1 = g.addV('String').property('text', text);
-
-  return await v1.next();
+  return await v1
+    .addE('canonical')
+    .to(canonicalStringV)
+    .iterate();
 }
 
 async function linkStrings(text1, text2, edgeLabel, props = {}) {
@@ -33,8 +37,22 @@ async function linkStrings(text1, text2, edgeLabel, props = {}) {
     e.property(key, props[key]);
   });
 
-  e.next();
+  e.iterate();
 }
+
+async function asyncPrompt(message) {
+  return await prompts({
+    type: 'confirm',
+    name: 'value',
+    message: message,
+    initial: true,
+  });
+}
+async function noopPrompt() {
+  return;
+}
+
+const prompt = noopPrompt;
 
 async function main() {
   console.log(`connecting`);
@@ -48,7 +66,7 @@ async function main() {
       .drop()
       .iterate();
 
-    const texts = [
+    const strings = [
       'estoy embarazada',
       'estoy avergonzada',
       'i am embarrassed',
@@ -57,45 +75,83 @@ async function main() {
       'embarazado',
       'embarazar',
       'avergonzado',
-    ];
-
-    const headwords = [
-      'estar',
       'i',
       'embarrass',
       'pregnant',
       'to be',
-      'embarazado',
-      'embarazar',
-      'avergonzado',
     ];
 
-    const gAddString = addString.bind(g);
-    texts.forEach(gAddString);
+    const headwords = [
+      { string: 'estar', lang: 'es' },
+      { string: 'embarazado', lang: 'es' },
+      { string: 'embarazar', lang: 'es' },
+      { string: 'avergonzado', lang: 'es' },
+      { string: 'i', lang: 'en' },
+      { string: 'embarrass', lang: 'en' },
+      { string: 'pregnant', lang: 'en' },
+      { string: 'to be', lang: 'en' },
+    ];
+
+    const gAddString = addStringV.bind(g);
+    strings.forEach(gAddString);
+
+    // Create headwords, linked to strings
+    const gAddHeadword = addHeadwordV.bind(g);
+    headwords.forEach(async ({ string, lang }) => {
+      const stringV = await g.V().has('String', 'text', string);
+      return await gAddHeadword({ lang }, stringV);
+    });
 
     const gLinkStrings = linkStrings.bind(g);
 
-    const gAddHeadword = addHeadword.bind(g)
-    headwords.forEach(gAddHeadword)
-
-    function linkStringsToHeadwords() {
-      // iterate through all headwords
-      // if headword is in a string, add a Contains edge from string to headword
-      // g.V().has('text', )
-    }
-
-    // await gLinkStringsToHeadwords()
-
-    await gLinkStrings('estoy embarazada', 'estoy avergonzada', 'Mistake', {
-      reason: 'false cognate',
-      explanation:
-        'Embarazada sounds like embarrassed to English speakers but it does not mean embarrassed.',
-    });
     await gLinkStrings('estoy avergonzada', 'estar', 'Contains');
     await gLinkStrings('estoy embarazada', 'estar', 'Contains');
     await gLinkStrings('estoy avergonzada', 'avergonzado', 'Contains');
-    const inbound = await g.V().has('String', 'text', 'estar').inE().toList()
-    console.log(`inbound relationships to estar`, inbound);
+    await gLinkStrings('i am embarrassed', 'i', 'Contains');
+    await gLinkStrings('i am embarrassed', 'to be', 'Contains');
+    await gLinkStrings('i am embarrassed', 'embarrass', 'Contains');
+    await gLinkStrings('i am pregnant', 'i', 'Contains');
+    await gLinkStrings('i am pregnant', 'to be', 'Contains');
+    await gLinkStrings('i am pregnant', 'pregnant', 'Contains');
+
+    // await prompt('Show inbound links');
+
+    // const inbound = await g
+    //   .V()
+    //   .has('String', 'text', 'estar')
+    //   .inE()
+    //   .toList();
+    // console.log(`inbound relationships to estar`, inbound);
+
+    await prompt('Add Mistake edge');
+
+    // await gLinkStrings('estoy embarazada', 'estoy avergonzada', 'Mistake', {
+    //   reason: 'false cognate',
+    //   explanation:
+    //     'Embarazada sounds like embarrassed to English speakers but it does not mean embarrassed.',
+    // });
+
+    const s1 = g.V().has('String', 'text', 'estoy embarazada');
+    const s2 = g.V().has('String', 'text', 'estoy avergonzada');
+    const e = s1
+      .addE('Mistake')
+      .to(s2)
+      .property('reason', 'false cognate')
+      .property(
+        'explanation',
+        'Embarazada sounds like embarrassed to English speakers but it does not mean embarrassed.'
+      );
+    await e.iterate();
+
+    await prompt('Show phrases with mistakes');
+
+    const mistakes = await g
+      .V()
+      .hasLabel('Mistake')
+      // .out()
+      .toList();
+    console.log(`phrases with mistakes`, mistakes);
+
     console.log(`done.`);
   } catch (ex) {
     console.error(ex);
